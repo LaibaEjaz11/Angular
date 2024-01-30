aed178d6925799822ea6d58267f1ba0a33243ec0
LaibaEjaz11 <laibaejaz144@gmail.com>
2024-01-30 11:05:55 +0500

    Update code with latest changes

---
+++ src/app/template-reference-variable/template-reference-variable.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-template-reference-variable',
+  standalone: true,
+  imports: [],
+  templateUrl: './template-reference-variable.component.html',
+  styleUrl: './template-reference-variable.component.css'
+})
+export class TemplateReferenceVariableComponent {
+
+}
